7719263b1eac41cfd7fc1d976bfaf512ba4e844f
mikekorakakis <mike.korakakis@gmail.com>
2023-07-06 14:12:39 +0300

    finshed bst

--- Data Structures/bst.js
+++ Data Structures/bst.js
@@ -1,3 +1,11 @@
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.left = null;
+		this.right = null;
+	}
+}
+
 class BinarySearchTree {
 	constructor() {
 		this.root = null;
@@ -20,7 +28,7 @@ class BinarySearchTree {
 				} else {
 					current = current.right;
 				}
-			} else if (val > current.val) {
+			} else if (val < current.val) {
 				if (current.left === null) {
 					current.left = node;
 					return this;
@@ -31,18 +39,27 @@ class BinarySearchTree {
 		}
 	}
 
-    find(val){
-        let current = this.root;
-        if(current === null) return null;
-        
-
-    }
-}
-
-class Node {
-	constructor(value) {
-		this.value = value;
-		this.left = null;
-		this.right = null;
+	find(val) {
+		let current = this.root;
+		if (current === null) return false;
+		let found = false;
+		while (current && !found) {
+			if (val > current.val) {
+				current = current.right;
+			} else if (val < current.val) {
+				current = current.left;
+			} else {
+				found = true;
+			}
+		}
+		if (!found) return undefined;
+		return current;
 	}
 }
+
+const tree = new BinarySearchTree();
+tree.insert(1);
+tree.insert(2);
+tree.insert(3);
+// console.log(tree)
+console.log(tree.find(5));
